285d3cfe997700e46e95e1d7d6371e5998eafa06
ronikimhi <155725608+ronikimhi@users.noreply.github.com>
2024-01-12 13:36:31 +0200

    Add files via upload

---
+++ Race/Track.java
@@ -0,0 +1,14 @@
+//Roni Kimhi 315298182
+package Race;
+
+public class Track{
+        private int finishedRacers;
+
+        public void setFinishedRacers(int finishedRacers){
+                this.finishedRacers=finishedRacers;
+        }
+
+        public int getFinishedRacers() {
+                return finishedRacers;
+        }
+}
